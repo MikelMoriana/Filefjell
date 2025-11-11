a3aeaea93707daee28709717c2652883c090cd37
Mikel Moriana Armendariz <151072409+MikelMoriana@users.noreply.github.com>
2025-11-11 15:22:07 +0100

    Created manual_positions

--- run.R
+++ run.R
@@ -9,52 +9,61 @@ tar_make(script = "_targets.R")
 
 vis_data <- tar_visnetwork(script = "_targets.R")
 edges <- vis_data$x$edges
+
+manual_positions <- tribble(
+  name,                              ~x,     ~y,
+  
+  # Functions
+  
+  
+  # Cleaning the files
+  "filefjell_1972_2009_file",      -100,       ,
+  "filefjell_summit_data_file",       0,       , 
+  "filefjell_2024_file",            100,       ,
+  # 2
+  "filefjell_1972_2009",           -100,       ,
+  "filefjell_summit_data",            0,       ,
+  "filefjell_2024",                 100,       ,
+  # 3
+  "filefjell_1972_2009_tidy",      -100,       ,
+  "filefjell_summit_data_tidy",       0,       ,
+  "filefjell_2024_tidy",            100,       ,
+  # 4
+  "filefjell_1972_2009_clean",      -50,       ,
+  "filefjell_2024_clean",            50,       ,
+  # 5
+  "filefjell_data_clean",             0,       ,
+  # 6
+  "distance_change_data",          -100,       ,
+  "richness_data",                    0,       ,
+  "species_turnover_data",          150,       ,
+  # 7
+  "distance_change_rate_mod",      -100,       ,
+  "richness_change_data",             0,       ,
+  "turnover_data",                  150,       ,
+  "colonizers_data",                250,       ,
+  # 8
+  "richness_change_rate_mod",         0,       ,
+  "lost_rate_mod",                  100,       ,
+  "new_rate_mod",                   200,       ,
+  # Others
+  "adj_label",                      200,       ,
+  "colour_mapping",                 300,       ,
+  "gg_modvars",                     200,       ,
+  "gg_yearline",                    300,       ,
+  "model_ch_factors",               400,       ,
+  "model_diagnosis",                500,       ,
+  "model_distribution",             600,       ,
+  "model_factors",                  700,       ,
+  "model_homoscedasticity",         800,       ,
+  "optimizer",                      900,       ,
+  "remove_terms",                  1000,       ,
+  "frequency_nmds_var",            1100,       
+
+)
+
 nodes <- vis_data$x$nodes |> 
-  mutate(x = level * 200) |>
-  mutate(y = case_when(
-    # 1
-    name == "filefjell_1972_2009_file" ~ -100, 
-    name == "filefjell_summit_data_file" ~ 0, 
-    name == "filefjell_2024_file" ~ 100, 
-    # 2
-    name == "filefjell_1972_2009" ~ -100, 
-    name == "filefjell_summit_data" ~ 0, 
-    name == "filefjell_2024" ~ 100, 
-    # 3
-    name == "filefjell_1972_2009_tidy" ~ -100, 
-    name == "filefjell_summit_data_tidy" ~ 0, 
-    name == "filefjell_2024_tidy" ~ 100, 
-    # 4
-    name == "filefjell_1972_2009_clean" ~ -50, 
-    name == "filefjell_2024_clean" ~ 50, 
-    # 5
-    name == "filefjell_data_clean" ~ 0, 
-    # 6
-    name == "distance_change_data" ~ -100, 
-    name == "richness_data" ~ 0, 
-    name == "species_turnover_data" ~ 150, 
-    # 7
-    name == "distance_change_rate_mod" ~ -100, 
-    name == "richness_change_data" ~ 0, 
-    name == "turnover_data" ~ 150, 
-    name == "colonizers_data" ~ 250, 
-    # 8
-    name == "richness_change_rate_mod" ~ 0, 
-    name == "lost_rate_mod" ~ 100, 
-    name == "new_rate_mod" ~ 200, 
-    # Others
-    name == "adj_label" ~ 200, 
-    name == "colour_mapping" ~ 300, 
-    name == "gg_modvars" ~ 200, 
-    name == "gg_yearline" ~ 300, 
-    name == "model_ch_factors" ~ 400, 
-    name == "model_diagnosis" ~ 500, 
-    name == "model_distribution" ~ 600, 
-    name == "model_factors" ~ 700, 
-    name == "model_homoscedasticity" ~ 800, 
-    name == "optimizer" ~ 900, 
-    name == "remove_terms" ~ 1000, 
-    name == "frequency_nmds_var" ~ 1100))
+  left_join(manual_positions, by = "name")
 
 visNetwork(nodes, edges) |> 
   visNodes() |> 
